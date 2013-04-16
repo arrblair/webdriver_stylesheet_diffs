f1bb3056748842191ef678298bd6d254e1325140
arrblair <ryan@livefyre.com>
2013-04-15 23:37:43 -0700

    initialize variables, loop, JS calls

---
+++ diff.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from sys import argv
+
+
+site_one = argv[1]
+site_two = argv[2]
+sites_to_compare = [site_one, site_two]
+driver = webdriver.Chrome()
+stylesheet_urls = []
+
+for site in sites_to_compare():
+    driver.get(site) 
+    stylesheets.append((w.execute_script('return document.styleSheets[0]["href"];')))    
+    return stylesheet_urls

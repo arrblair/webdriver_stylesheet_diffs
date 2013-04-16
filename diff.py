c3d1521244f2ad84dd02dd4ec3c17aca00aecf25
arrblair <ryan@livefyre.com>
2013-04-16 05:56:17 -0700

    open file, save css as .css for formatting, and close

--- diff.py
+++ diff.py
@@ -14,7 +14,17 @@ for site in sites_to_compare():
     stylesheet_urls.append((driver.execute_script('return document.styleSheets[0]["href"];')))    
     stylesheet = (driver.execute_script('return document.styleSheets[0]["href"];'))
     driver.get(stylesheet)
+    split_current_url = driver.current_url
+    length = len(split_current_url)
+    filename = split_current_url[length-1]
+    if 'css' in filename[-3:]:
+        print "That's a css file already: %s" % filename
+    else:
+        filename += '.css' 
     css = driver.page_source
+    new_css_file = open(filename, "w")
+    new_css_file.write(css)
+    new_css_file.close()
     return stylesheet_urls
 
 
